d3b7f1e94a50daa5f2ad1f215eedb77aef8d4c1a
Hunter Godina <huntergodina@gmail.com>
2022-03-30 13:51:24 -0500

    Updated view and countdown

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -62,7 +62,7 @@ class Dashboard extends React.Component{
         <Row style={{display: "inline-block"}}>
             <div className="countdown">
                 <span>🚀</span>
-                <Countdown date={new Date("Feb 26, 2022 23:45:00")} daysInHours={true}/>
+                <Countdown date={new Date("Apr 2, 2022 22:00:00")} daysInHours={true}/>
                 <span style={{padding: 0}}> until OMAthon reveal!</span>
                 <span>🚀</span>
             </div>
